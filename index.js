d3236de75342558fb61b7b9ca5f491d517312575
aelmore2 <aelmore2@terpmail.umd.edu>
2025-05-07 17:40:29 -0400

    feat: Initial commit

---
+++ index.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const supbaseClient = require("@supabase/supabase-js");
+const bodyParser = require("body-parser");
+const { isValidStateAbbreviation } = require("usa-state-validator");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+app.use(express.static(__dirname + "/public"));
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
+const supabase = supbaseClient.createClient(supabaseUrl, supabaseKey);
+
+app.get("/customer", async (req, res) => {
+  console.log("Attempting to GET all customers!");
+
+  const { data, error } = await supabase.from("customer").select();
+
+  if (error) {
+    console.log(`Error: ${error}`);
+    res.statusCode = 500;
+    res.send(error);
+  }
+
+  res.send(data);
+});
+
+app.post("/customer", async (req, res) => {
+  console.log("Adding Customer");
+
+  console.log(req.body);
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+  const state = req.body.state;
+
+  if (!isValidStateAbbreviation(state)) {
+    console.log(`State ${state} is Invalid`);
+    res.statusCode = 400;
+    res.header("Content-Type", "application/json");
+    var errorJson = {
+      message: `${state} is not a Valid State`,
+    };
+    res.send(JSON.stringify(errorJson));
+    return;
+  }
+
+  const { data, error } = await supabase
+    .from("customer")
+    .insert({
+      customer_first_name: firstName,
+      customer_last_name: lastName,
+      customer_state: state,
+    })
+    .select();
+
+  if (error) {
+    console.log(`Error: ${error}`);
+    res.statusCode = 500;
+    res.send(error);
+  }
+
+  res.send(data);
+});
+
+app.listen(port, () => {
+  console.log("APP IS ALIVE ON PORT", port);
+});
